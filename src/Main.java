2b0fc6d0d235f576622a67242919279bf1e610c4
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-23 18:00:30 -0500

    creating objects with class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Doctor myDoctor = new Doctor(); //Instanciazion
+        myDoctor.name = "Alejandro Ching";
+        myDoctor.speciality = "Cirujano estetico";
+        myDoctor.showName();
+        myDoctor.showSpeciality();
     }
 }

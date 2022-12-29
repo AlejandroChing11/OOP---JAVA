6b82048d2496f7cbb13991e553d4e69a575057df
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-28 20:56:22 -0500

    Interfaces v2

---
+++ src/model/AppointmentNurse.java
@@ -0,0 +1,11 @@
+package model;
+import java.util.Date;
+
+public class AppointmentNurse {
+    private int id;
+    private Nurse nurse;
+    private Paciente paciente;
+    private Doctor doctor;
+    private Date date;
+    private String time;
+}

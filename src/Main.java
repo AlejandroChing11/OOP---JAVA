12bedffb93551ef493cdbd8618cade142356f683
Alejandro Ching <alejandroching2004@hotmail.com>
2022-12-28 20:21:22 -0500

    toString method v2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-        /*Doctor myDoctor = new Doctor(); //Instanciazion
-
-
-        Doctor myDoctorAlejo = new Doctor();
+        Doctor myDoctor = new Doctor("Sandra", "sandrafb@hotmail.com"); //Instanciazion
 
         //showMenu();
 
@@ -17,9 +14,15 @@ public class Main {
 
         for (Doctor.AvailableAppointment availableAppointment : myDoctor.getAvailableAppointments()) {
             System.out.println(availableAppointment.getDate() + " " + availableAppointment.getTime());
-        }*/
+        }
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+
         Paciente paciente = new Paciente("Alejandro", "alejandroching2004@hotmail.com");
         System.out.println(paciente);
+        System.out.println("");
+        System.out.println(myDoctor);
     }
 
 }

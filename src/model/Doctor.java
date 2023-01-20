0c5115b626e2833a6168666a843ebf7d76d42f60
Alejandro Ching <alejandroching2004@hotmail.com>
2023-01-20 17:47:14 -0500

    Auth Patient Menu v2

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 public class Doctor extends User {
     //Unique Atribute
     private String speciality;
+    private ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
 
     public Doctor(String name, String email){
         super(name, email);
@@ -26,7 +27,7 @@ public class Doctor extends User {
     }
 
 
-    ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
     public void addAvailableAppointment(String date, String time) {
         availableAppointments.add(new Doctor.AvailableAppointment(date, time));
     }
@@ -69,11 +70,11 @@ public class Doctor extends User {
             this.id = id;
         }
 
-        public Date getDate() {
+        public Date getDate(String DATE) {
             return date;
         }
 
-        public String getDate(String DATE) {
+        public String getDate() {
             return format.format(date);
         }
 
--- src/ui/UIPacienteMenu.java
+++ src/ui/UIPacienteMenu.java
@@ -1,6 +1,12 @@
 package ui;
 
+import model.Doctor;
+
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.ArrayList;
+
 
 public class UIPacienteMenu {
     public static void showPacienteMenu() {
@@ -33,6 +39,35 @@ public class UIPacienteMenu {
         do {
             System.out.println(":: Book an Appointment");
             System.out.println(":: Select a Date");
+            //Interger => lista de fechas
+            //Map<Integer, Doctor> => Indice fecha seleccionada
+            //[Doctors]
+            //1. -doctor1
+                // - 1 fecha1
+                // - 2 fecha2
+                // - 3 fecha3
+            //2. -doctor2
+                // - 1 fecha1
+                // - 2 fecha2
+                // - 3 fecha3
+            //3. -doctor3
+                // - 1 fecha1
+                // - 2 fecha2
+                // - 3 fecha3
+            Map<Integer, Map<Integer, Doctor>> doctors = new TreeMap<>();
+            int k = 0;
+            for (int i = 0; i < UIDoctorMenu.doctorsAvailableAppointments.size(); i++) {
+                ArrayList<Doctor.AvailableAppointment> availableAppointments = UIDoctorMenu.doctorsAvailableAppointments.get(i).getAvailableAppointments();
+
+                Map<Integer, Doctor> doctorAppointments = new TreeMap<>();
+
+                for (int j = 0; j < availableAppointments.size(); j++) {
+                    k++;
+                    System.out.println(k + ". " + availableAppointments.get(j).getDate());
+                    doctorAppointments.put(Integer.valueOf(j), UIDoctorMenu.doctorsAvailableAppointments.get(i));
+                    doctors.put(Integer.valueOf(k), doctorAppointments);
+                }
+            }
         } while(response != 0);
     }
 }
